f3ab0e789e2a1711581bf5931f66fda793dd7837
ch4ne5teban <ca1505pr@outlook.com>
2024-07-11 23:29:03 +0530

    Add solution to UTF16 string value problem

---
+++ js101-js119-small-problems/ez1-UTF16-string-value.js
@@ -0,0 +1,10 @@
+//Write a function that determines and returns the UTF-16 string value of a string passed in as an argument. The UTF-16 string value is the sum of the UTF-16 values of every character in the string. (You may use String.prototype.charCodeAt() to determine the UTF-16 value of a character.)
+
+function utf16Value(str) {
+  return str.split('').reduce((sum, char) => sum + char.charCodeAt(0), 0);
+}
+
+console.log(utf16Value('Four score'));         // 984
+console.log(utf16Value('Launch School'));      // 1251
+console.log(utf16Value('a'));                  // 97
+console.log(utf16Value(''));                   // 0
