247693f8edb21f32c2e9a6c41d8ca3e96109edef
ch4ne5teban <ca1505pr@outlook.com>
2024-07-12 17:32:32 +0530

    Add Rock Paper Scissors Lizard Spock app.js and messages.json

---
+++ lesson_2/rock_paper_scissors/app.js
@@ -0,0 +1,149 @@
+const READLINE = require("readline-sync");
+const MESSAGES = require("./messages.json");
+
+const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
+const USER_CHOICE_MAP = {
+  1: "rock", r: "rock", rock: "rock",
+  2: "paper", p: "paper", paper: "paper",
+  3: "scissors", sc: "scissors", scissors: "scissors",
+  4: "lizard", l: "lizard", lizard: "lizard",
+  5: "spock", sp: "spock", spock: "spock"
+};
+const WINNING_SCENARIOS = ["rockscissors", "rocklizard", "paperspock", "paperrock", "scissorspaper", "scissorslizard", "spockscissors", "spockrock", "lizardspock", "lizardpaper"];
+
+let currentLang = getUserLanguage();
+
+function distinctivePrompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function getUserLanguage() {
+  console.clear();
+  distinctivePrompt(MESSAGES['welcome']);
+  displayLanguages();
+  let langChoice = parseInt(READLINE.question(), 10) - 1;
+  const languages = Object.keys(MESSAGES.languages);
+  while (isNaN(langChoice) ||
+  langChoice < 0 ||
+  langChoice >= languages.length) {
+    console.clear();
+    distinctivePrompt(MESSAGES['invalid_language_choice']);
+    langChoice = parseInt(READLINE.question(), 10) - 1;
+  }
+  return languages[langChoice];
+}
+
+function displayLanguages() {
+  const languages = Object.keys(MESSAGES.languages);
+  languages.forEach((lang, index) => {
+    distinctivePrompt(`${index + 1} for ${lang.toUpperCase()}`);
+  });
+}
+
+function displayRules() {
+  console.clear();
+  console.log(MESSAGES.languages[currentLang]['rules']);
+}
+
+function getUserChoice() {
+  distinctivePrompt(MESSAGES.languages[currentLang]['options']);
+  let choice = READLINE.question().toLowerCase().trim();
+  while (!USER_CHOICE_MAP.hasOwnProperty(choice)) {
+    distinctivePrompt(MESSAGES.languages[currentLang]['invalid_choice']);
+    choice = READLINE.question().toLowerCase().trim();
+  }
+  return USER_CHOICE_MAP[choice];
+}
+
+function getComputerChoice() {
+  console.clear();
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  return VALID_CHOICES[randomIndex];
+}
+
+function gameLogic(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'tie';
+  }
+  let scenario = userChoice.concat(computerChoice);
+  if (WINNING_SCENARIOS.includes(scenario)) {
+    return 'win';
+  } else {
+    return 'lose';
+  }
+}
+
+function playSingleRound() {
+  let userChoice = getUserChoice();
+  let compChoice = getComputerChoice();
+  distinctivePrompt(`You chose ${userChoice}, computer chose ${compChoice}`);
+  let singleRoundOutcome = gameLogic(userChoice, compChoice);
+  return singleRoundOutcome;
+}
+
+function displaySingleRoundOutcome(singleRoundOutcome) {
+  if (singleRoundOutcome === 'win') {
+    distinctivePrompt(MESSAGES.languages[currentLang]['user_wins_round']);
+  } else if (singleRoundOutcome === 'lose') {
+    distinctivePrompt(MESSAGES.languages[currentLang]['computer_wins_round']);
+  } else {
+    distinctivePrompt(MESSAGES.languages[currentLang]['tie_round']);
+  }
+}
+
+function updateScores(singleRoundOutcome, userWins, computerWins) {
+  if (singleRoundOutcome === 'win') {
+    userWins++;
+  } else if (singleRoundOutcome === 'lose') {
+    computerWins++;
+  }
+  return [userWins, computerWins];
+}
+
+function playThreeRounds() {
+  let userWins = 0;
+  let computerWins = 0;
+  for (let round = 0; round < 3; round++) {
+    let singleRoundOutcome = playSingleRound();
+    [userWins, computerWins] =
+    updateScores(singleRoundOutcome, userWins, computerWins);
+    displaySingleRoundOutcome(singleRoundOutcome);
+  }
+  displayFinalGameOutcome(userWins, computerWins);
+}
+
+function displayFinalGameOutcome(userWins, computerWins) {
+  if (userWins > computerWins) {
+    distinctivePrompt(`Final Score: You won ${userWins} ${(userWins === 1) ? "round" : "rounds"}. ${(computerWins === 0) ? "Computer didn't win any round" : "Computer won 1 round"}.`);
+    distinctivePrompt(MESSAGES.languages[currentLang]['win_game']);
+  } else if (computerWins > userWins) {
+    distinctivePrompt(`Final Score: Computer won ${computerWins} ${(computerWins === 1) ? "round" : "rounds"}. ${(userWins === 0) ? "You didn't win any round" : "You won 1 round"}.`);
+    distinctivePrompt(MESSAGES.languages[currentLang]['lose_game']);
+  } else {
+    distinctivePrompt(MESSAGES.languages[currentLang]['tie_game']);
+  }
+}
+
+function userWantsAnotherRound() {
+  distinctivePrompt(MESSAGES.languages[currentLang]['another_round']);
+  let userDecision = READLINE.question().toLowerCase();
+  while (!MESSAGES.languages[currentLang]['yes'].includes(userDecision) && !MESSAGES.languages[currentLang]['no'].includes(userDecision)) {
+    console.clear();
+    distinctivePrompt(MESSAGES.languages[currentLang]['yes_no_prompt']);
+    userDecision = READLINE.question().toLowerCase();
+  }
+  console.clear();
+  return MESSAGES.languages[currentLang]['yes'].includes(userDecision);
+}
+
+function runGame() {
+  do {
+    displayRules();
+    playThreeRounds();
+  } while (userWantsAnotherRound());
+  console.clear();
+  distinctivePrompt(MESSAGES.languages[currentLang]['thanks']);
+}
+
+runGame();
+
