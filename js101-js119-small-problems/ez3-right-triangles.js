9219641085576255b46f00700e8a841cff53bb12
ch4ne5teban <ca1505pr@outlook.com>
2024-07-28 14:45:00 +0530

    Add solution to ez3 right triangles

---
+++ js101-js119-small-problems/ez3-right-triangles.js
@@ -0,0 +1,27 @@
+//Write a function that takes a positive integer, n, as an argument and logs a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+function triangle(int) {
+  for (let i = 1; i <= int; i++) {
+     console.log(" ".repeat(int - i) + "*".repeat(i));
+  }
+}
+
+triangle(5);
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+triangle(9);
+
+//         *
+//        **
+//       ***
+//      ****
+//     *****
+//    ******
+//   *******
+//  ********
+// *********
