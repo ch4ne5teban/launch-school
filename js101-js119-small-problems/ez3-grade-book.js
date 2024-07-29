3c5449d09902ff8c56d0f41e7bfb32329e1e70fc
ch4ne5teban <ca1505pr@outlook.com>
2024-07-29 15:19:24 +0530

    Add solution to ez3 grade book

---
+++ js101-js119-small-problems/ez3-grade-book.js
@@ -0,0 +1,33 @@
+/*
+Write a function that determines the mean (average) of the three scores passed to it, and returns the letter associated with that grade.
+
+Numerical score letter grade list:
+
+90 <= score <= 100: 'A'
+80 <= score < 90: 'B'
+70 <= score < 80: 'C'
+60 <= score < 70: 'D'
+0 <= score < 60: 'F'
+Tested values are all between 0 and 100. There is no need to check for negative values or values greater than 100.
+*/
+
+function getGrade(numScore1, numScore2, numScore3) {
+  let meanScore = (numScore1 + numScore2 + numScore3) / 3;
+  
+  if (meanScore >= 90) {
+    return 'A';
+  } else if (meanScore >= 80) {
+      return 'B';
+  } else if (meanScore >= 70) {
+      return 'C';
+  } else if (meanScore >= 60) {
+      return 'D';
+  } else {
+      return 'F';
+  }
+}
+
+//`switch` doesn't do range-based comparisons
+
+console.log(getGrade(95, 90, 93));    // "A"
+console.log(getGrade(50, 50, 95));    // "D"
