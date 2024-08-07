c8c86a8b1ff6fbcce1a3e2fc178dde435b5fb477
ch4ne5teban <ca1505pr@outlook.com>
2024-08-08 03:53:03 +0530

    Add solution to after before midnight

---
+++ js101-js119-small-problems/ez5-after-before-midnight.js
@@ -0,0 +1,28 @@
+/*
+As seen in the previous exercise, the time of day can be represented as the number of minutes before or after midnight. If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+Write two functions that each take a time of day in 24 hour format, and return the number of minutes before and after midnight, respectively. Both functions should return a value in the range 0..1439.
+
+You may not use javascript's Date class methods.
+*/
+
+function afterMidnight(time) {
+  let hoursMinutes = time.split(':');
+  let minutes = (hoursMinutes[0] * 60) + Number(hoursMinutes[1]);
+
+  return minutes % 1440;
+}
+
+function beforeMidnight(time) {
+  let hoursMinutes = time.split(':');
+  let minutes = 1440 - ((hoursMinutes[0] * 60) + Number(hoursMinutes[1]));
+
+  return minutes % 1440;
+}
+
+console.log(afterMidnight("00:00") === 0);
+console.log(beforeMidnight("00:00") === 0);
+console.log(afterMidnight("12:34") === 754);
+console.log(beforeMidnight("12:34") === 686);
+console.log(afterMidnight("24:00") === 0);
+console.log(beforeMidnight("24:00") === 0);
