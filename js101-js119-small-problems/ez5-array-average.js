a88c1dbacddfd4b88deaffbb477ca385c64db438
ch4ne5teban <ca1505pr@outlook.com>
2024-08-07 20:02:44 +0530

    Add solution to array average

---
+++ js101-js119-small-problems/ez5-array-average.js
@@ -0,0 +1,8 @@
+//Write a function that takes one argument, an array of integers, and returns the average of all the integers in the array, rounded down to the integer component of the average. The array will never be empty, and the numbers will always be positive integers.
+
+function average(integers) {
+  return Math.floor(integers.reduce((acc, int) => acc + int) / integers.length);
+}
+
+console.log(average([1, 5, 87, 45, 8, 8]));       // 25
+console.log(average([9, 47, 23, 95, 16, 52]));    // 40
