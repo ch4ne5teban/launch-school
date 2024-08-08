03b0f49266053458e7a6bc2c06fd71e640aaf5c5
ch4ne5teban <ca1505pr@outlook.com>
2024-08-08 22:44:46 +0530

    Add solution to double char 1

---
+++ js101-js119-small-problems/ez6-double-char-1.js
@@ -0,0 +1,21 @@
+//Write a function that takes a string, doubles every character in the string, and returns the result as a new string.
+
+//Using split(), map(), and join()
+function repeater(string) {
+  return string.split('').map((char) => char + char).join('');
+}
+
+//Using an empty array and for loop
+function repeater(string) {
+  let repeaterArr = [];
+
+  for (let i = 0; i < string.length; i++) {
+    repeaterArr.push(string[i] + string[i]);
+  }
+
+  return repeaterArr.join('');
+}
+
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""
